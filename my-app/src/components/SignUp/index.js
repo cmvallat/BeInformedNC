71dbad65d13243ba2612d7ae6c94b3cb8695d717
wsrogers <wsrogers@users.noreply.github.com>
2020-06-30 14:26:52 -0400

    Sign up completed

--- my-app/src/components/SignUp/index.js
+++ my-app/src/components/SignUp/index.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { FirebaseContext } from '../Firebase';
-import * as ROUTES from '../../constants/routes';
+import { Link, withRouter } from 'react-router-dom';
+import { compose } from 'recompose';
 
-const SignUp = () => (
+import { withFirebase } from '../Firebase';
+import * as ROUTES from '../../constants/routes';
+import * as ROLES from '../../constants/roles';
+
+const SignUpPage = () => (
     <div>
         <h1>SignUp</h1>
-        <FirebaseContext.Consumer>
-            {firebase => <SignUpForm firebase={firebase} />}
-        </FirebaseContext.Consumer>
+        <SignUpForm />
     </div>
 );
 
@@ -17,31 +18,69 @@ const INITIAL_STATE = {
     email: '',
     passwordOne: '',
     passwordTwo: '',
+    isAdmin: false,
     error: null,
 };
 
-class SignUpForm extends Component {
+const ERROR_CODE_ACCOUNT_EXISTS = 'auth/email-already-in-use';
+
+const ERROR_MSG_ACCOUNT_EXISTS = `
+  An account with this E-Mail address already exists.
+  Try to login with this account instead. If you think the
+  account is already used from one of the social logins, try
+  to sign in with one of them. Afterward, associate your accounts
+  on your personal account page.
+`;
+
+class SignUpFormBase extends Component {
     constructor(props) {
         super(props);
-        this.state ={ ...INITIAL_STATE}
+
+        this.state = { ...INITIAL_STATE };
     }
 
     onSubmit = event => {
-        const { name, email, passwordOne } = this.state;
+        const { name, email, passwordOne, isAdmin } = this.state;
+        const roles = {};
+
+        if (isAdmin) {
+            roles[ROLES.ADMIN] = ROLES.ADMIN;
+        }
 
         this.props.firebase
             .doCreateUserWithEmailAndPassword(email, passwordOne)
             .then(authUser => {
+                // Create a user in your Firebase realtime database
+                return this.props.firebase.user(authUser.user.uid).set({
+                    name,
+                    email,
+                    roles,
+                });
+            })
+            .then(() => {
+                return this.props.firebase.doSendEmailVerification();
+            })
+            .then(() => {
                 this.setState({ ...INITIAL_STATE });
+                this.props.history.push(ROUTES.HOME);
             })
             .catch(error => {
+                if (error.code === ERROR_CODE_ACCOUNT_EXISTS) {
+                    error.message = ERROR_MSG_ACCOUNT_EXISTS;
+                }
+
                 this.setState({ error });
             });
 
         event.preventDefault();
-    }
+    };
+
     onChange = event => {
         this.setState({ [event.target.name]: event.target.value });
+    };
+
+    onChangeCheckbox = event => {
+        this.setState({ [event.target.name]: event.target.checked });
     };
 
     render() {
@@ -50,6 +89,7 @@ class SignUpForm extends Component {
             email,
             passwordOne,
             passwordTwo,
+            isAdmin,
             error,
         } = this.state;
 
@@ -89,19 +129,36 @@ class SignUpForm extends Component {
                     type="password"
                     placeholder="Confirm Password"
                 />
+                <label>
+                    Admin:
+                    <input
+                        name="isAdmin"
+                        type="checkbox"
+                        checked={isAdmin}
+                        onChange={this.onChangeCheckbox}
+                    />
+                </label>
                 <button disabled={isInvalid} type="submit">
                     Sign Up
                 </button>
+
                 {error && <p>{error.message}</p>}
             </form>
         );
     }
 }
+
 const SignUpLink = () => (
     <p>
         Don't have an account? <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
     </p>
 );
 
-export default SignUp;
+const SignUpForm = compose(
+    withRouter,
+    withFirebase,
+)(SignUpFormBase);
+
+export default SignUpPage;
+
 export { SignUpForm, SignUpLink };
